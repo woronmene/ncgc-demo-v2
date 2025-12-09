e03a21a773168858aa460b63e26e60263281a40c
Woron Mene <meneworon4@gmail.com>
2025-12-09 11:12:25 +0100

    fix useSearchParams issue

--- app/ncgc_analyst/recovery/create/page.jsx
+++ app/ncgc_analyst/recovery/create/page.jsx
@@ -1,16 +1,9 @@
-"use client";
 import { Suspense } from "react";
 import CreateRecoveryPage from "./CreateRecoveryPage";
 
 export default function RecoveryCreatePage() {
   return (
-    <Suspense
-      fallback={
-        <div className="p-10 flex justify-center">
-          <Loader2 className="animate-spin text-gray-400" size={32} />
-        </div>
-      }
-    >
+    <Suspense fallback={<div>Loading recovery create page...</div>}>
       <CreateRecoveryPage />
     </Suspense>
   );
